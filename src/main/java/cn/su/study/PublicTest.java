6bcccd799376f59ca29a8da197954fdd6d322a44
suyulong <hiro.syl@alibaba-inc.com>
2018-11-15 19:38:57 +0800

    daily same test

---
+++ src/main/java/cn/su/study/PublicTest.java
@@ -0,0 +1,140 @@
+package cn.su.study;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author suyulong
+ * @date 2018/5/14 16:04
+ */
+public class PublicTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Oops, cannot get logQueryUrl, please use this command 'WAIT instanceId;' in the console to see additional details!");
+        //List<String> list = new ArrayList<String>();
+        //list.add("a");
+        //list.add("b");
+        //list.add("c");
+        //String str = StringUtils.join(list.toArray(), ",");
+        //System.out.println(str); ////a,b,c
+        //
+        //
+        //String str1 = "a,b,c";
+        //List<String> result = Arrays.asList(StringUtils.split(str1,","));
+        //System.out.println(result);
+
+        //System.out.println(excludeRuleId("1,2","1"));
+        //
+        //System.out.println(excludeRuleId("1","1"));
+        //
+        //System.out.println(excludeRuleId("1,2,2,2","2"));
+
+
+        /*Thread t = new Thread(()-> System.out.println(1/0));
+        t.start();
+        t.join();
+*/
+        //java中整数默认int，小数默认double
+        //fload f = 3.4 //就是错误的，类型不匹配
+        //float f = 3.4f;
+        //
+        //String a = new String("asdf");
+        //String b = new String("asdf");
+        //String f = "asdf";
+        //System.out.println(b == a);
+        //System.out.println(f == a);
+        //a = a.intern();
+        //System.out.println(b == a);
+        //System.out.println(f == a);
+        //b = b.intern();
+        //System.out.println(b == a);
+
+        //System.out.println(1 & 2);
+
+        //System.out.println(2 & 2);
+
+        /*for (; ; ) {
+
+            Random random = new Random();
+            int result;
+            result = random.nextInt(2);
+            //result = (result < 1) ? 1 : result;
+            System.out.println(result);
+        }*/
+
+        //PublicTest publicTest = new PublicTest();
+        ////publicTest.printMethodInClass();
+        //
+        //String tableVO = null, name = null;
+        //try {
+        //    publicTest.getParamName(tableVO, name);
+        //} catch (NoSuchMethodException e) {
+        //    e.printStackTrace();
+        //}
+
+
+        /*List<String> a = new ArrayList<>(Arrays.asList("1","2","3"));
+        List<String> b = new ArrayList<>();
+        b.addAll(a);
+        //Iterator<String> iterator = a.iterator();
+        //while (iterator.hasNext()){
+        //    iterator.next();
+        //    iterator.remove();
+        //}
+        System.out.println(a);
+        System.out.println(b);
+
+        Optional<String> any = b.stream().filter(x -> !a.contains(x)).findFirst();
+        if(any.isPresent()){
+            System.out.println("haha");
+        }*/
+    }
+
+    private static List<String> excludeRuleId(String origRules, String excludeId) {
+        return Arrays.asList(StringUtils.split(origRules, ",")).stream().filter(
+            id -> !id.equals(excludeId)).collect(Collectors.toList());
+    }
+
+    /**
+     * 方法名：getParamName 参数类型：[Ljava.lang.String;
+     * <p>
+     * 虽然封装成了数组类型，但反射不支持可变参数的执行
+     *
+     * @param args
+     * @throws NoSuchMethodException
+     */
+    public void getParamName(String... args) throws NoSuchMethodException {
+        DefaultParameterNameDiscoverer discover = new DefaultParameterNameDiscoverer();
+        String[] parameterNames = discover.getParameterNames(this.getClass()
+            .getMethod("getParamName", String[].class));
+        for (String param : parameterNames) {
+            //只能打印成：args
+            System.out.println(param);
+        }
+    }
+
+    public void printMethodInClass() {
+        Class<?> clazz = this.getClass();
+        //获取本类的所有方法，存放入数组
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println("方法名：" + method.getName());
+            //获取本方法所有参数类型，存入数组
+            Class<?>[] getTypeParameters = method.getParameterTypes();
+            if (getTypeParameters.length == 0) {
+                System.out.println("此方法无参数");
+            }
+            for (Class<?> class1 : getTypeParameters) {
+                String parameterName = class1.getName();
+                System.out.println("参数类型：" + parameterName);
+            }
+            System.out.println("****************************");
+        }
+
+    }
+
+}
--- src/main/java/cn/su/study/proxy/DynamicProxyPerformanceTest.java
+++ src/main/java/cn/su/study/proxy/DynamicProxyPerformanceTest.java
@@ -1,225 +1,225 @@
-package cn.su.study.proxy;
-
-import javassist.*;
-import javassist.util.proxy.MethodHandler;
-import javassist.util.proxy.ProxyFactory;
-import javassist.util.proxy.ProxyObject;
-import org.springframework.cglib.proxy.Enhancer;
-import org.springframework.cglib.proxy.MethodInterceptor;
-import org.springframework.cglib.proxy.MethodProxy;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.text.DecimalFormat;
-
-/**
- * @author suyulong
- * @date 2018/5/22 16:01
- */
-public class DynamicProxyPerformanceTest {
-
-    /**
-     *
-     * Create JDK Proxy: 7 ms
-     * Create CGLIB Proxy: 100 ms
-     * Create JAVAASSIST Proxy: 88 ms
-     * Create JAVAASSIST Bytecode Proxy: 59 ms
-     * ================
-     * Run JDK Proxy: 44 ms, 32,046,941 t/s
-     * Run CGLIB Proxy: 169 ms, 8,343,582 t/s
-     * Run JAVAASSIST Proxy: 211 ms, 6,682,774 t/s
-     * Run JAVAASSIST Bytecode Proxy: 52 ms, 27,116,642 t/s
-     *
-     * @param args
-     * @throws Exception
-     */
-
-    public static void main(String[] args) throws Exception {
-        CountService delegate = new CountServiceImpl();
-
-        long time = System.currentTimeMillis();
-        CountService jdkProxy = createJdkDynamicProxy(delegate);
-        time = System.currentTimeMillis() - time;
-        System.out.println("Create JDK Proxy: " + time + " ms");
-
-        time = System.currentTimeMillis();
-        CountService cglibProxy = createCglibDynamicProxy(delegate);
-        time = System.currentTimeMillis() - time;
-        System.out.println("Create CGLIB Proxy: " + time + " ms");
-
-        time = System.currentTimeMillis();
-        CountService javassistProxy = createJavaAssistDynamicProxy(delegate);
-        time = System.currentTimeMillis() - time;
-        System.out.println("Create JAVAASSIST Proxy: " + time + " ms");
-
-        time = System.currentTimeMillis();
-        CountService javassistBytecodeProxy = createJavaAssistByteCodeDynamicProxy(delegate);
-        time = System.currentTimeMillis() - time;
-        System.out.println("Create JAVAASSIST Bytecode Proxy: " + time + " ms");
-
-        System.out.println("================");
-
-        for (int i = 0; i < 3; i++) {
-            test(jdkProxy, "Run JDK Proxy: ");
-            test(cglibProxy, "Run CGLIB Proxy: ");
-            test(javassistProxy, "Run JAVAASSIST Proxy: ");
-            test(javassistBytecodeProxy, "Run JAVAASSIST Bytecode Proxy: ");
-            System.out.println("----------------");
-        }
-
-    }
-
-    private static void test(CountService service, String label)
-        throws Exception {
-        service.count(); // warm up
-        int count = 10000000;
-        long time = System.currentTimeMillis();
-        for (int i = 0; i < count; i++) {
-            service.count();
-        }
-        time = System.currentTimeMillis() - time;
-        System.out.println(label + time + " ms, " + new DecimalFormat().format(count * 1000 / time) + " t/s");
-    }
-
-
-    /**
-     * jdk dynamic proxy
-     * <p>
-     * JDK的动态代理是靠反射完成的 一定要实现接口
-     *
-     * @param delegate
-     * @return
-     */
-    private static CountService createJdkDynamicProxy(final CountService delegate) {
-        CountService countService_jdk = (CountService)Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(),
-            new Class[] {CountService.class},
-            new JdkHandler(delegate));
-        return countService_jdk;
-    }
-
-    private static class JdkHandler implements InvocationHandler {
-
-        final Object delegate;
-
-        private JdkHandler(Object delegate) {this.delegate = delegate;}
-
-        @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            //long time = System.currentTimeMillis();
-            Object o = method.invoke(delegate, args);
-            //time = System.currentTimeMillis() - time;
-            //System.out.println("Create JDK Proxy: " + time + " ms");
-            return o;
-        }
-    }
-
-    /*****************************/
-
-    /**
-     * cglib dynamic proxy
-     * <p>
-     * cglib原理是让目标类生成一个子类，然后让子类去进行方法的增强
-     * <p>
-     * final Class不支持, 因为CGLIB是生成子类来实现AOP,所以final Class自然无法支持了. 需要强制无参数构造函数
-     *
-     * @param delegate
-     * @return
-     */
-    private static CountService createCglibDynamicProxy(final CountService delegate) {
-        Enhancer enhancer = new Enhancer();
-        enhancer.setCallback(new CglibInterceptor(delegate));
-        enhancer.setInterfaces(new Class[] {CountService.class});
-        CountService cglibProxy = (CountService)enhancer.create();
-        return cglibProxy;
-    }
-
-    /**
-     * MethodInterceptor接口只定义了一个方法：
-     * <p>
-     * public Object intercept(Object object, java.lang.reflect.Method method, Object[] args, MethodProxy proxy) throws
-     * Throwable;
-     * <p>
-     * 参数Object object是被代理对象，不会出现死循环的问题。
-     * <p>
-     * 参数java.lang.reflect.Method method是java.lang.reflect.Method类型的被拦截方法。
-     * <p>
-     * 参数Object[] args是被被拦截方法的参数。
-     * <p>
-     * 参数MethodProxy proxy是CGLIB提供的MethodProxy 类型的被拦截方法。
-     * <p>
-     * 注意：
-     * <p>
-     * 1、若原方法的参数存在基本类型，则对于第三个参数Object[] args会被转化成类的类型。如原方法的存在一个参数为int，则在intercept方法中，对应的会存在一个Integer类型的参数。
-     * <p>
-     * 2、若原方法为final方法，则MethodInterceptor接口无法拦截该方法
-     */
-    private static class CglibInterceptor implements MethodInterceptor {
-        final Object delegate;
-
-        private CglibInterceptor(Object delegate) {this.delegate = delegate;}
-
-        @Override
-        public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-            //long time = System.currentTimeMillis();
-            Object obj = method.invoke(delegate, objects);
-            //time = System.currentTimeMillis() - time;
-            //System.out.println("Create CGLib Proxy: " + time + " ms");
-            return obj;
-        }
-    }
-
-    /****************************/
-
-    /**
-     * JavaAssist 类似jdk接口实现 动态代理
-     */
-
-    private static class JavaAssistInterceptor implements MethodHandler {
-        final Object delegate;
-
-        private JavaAssistInterceptor(Object delegate) {this.delegate = delegate;}
-
-        @Override
-        public Object invoke(Object o, Method method, Method method1, Object[] objects) throws Throwable {
-            //long time = System.currentTimeMillis();
-            Object obj = method.invoke(delegate, objects);
-            //time = System.currentTimeMillis() - time;
-            //System.out.println("Create JAVAASSIST Proxy: " + time + " ms");
-            return obj;
-        }
-    }
-
-    private static CountService createJavaAssistDynamicProxy(final CountService delegate)
-        throws IllegalAccessException, InstantiationException {
-        ProxyFactory proxyFactory = new ProxyFactory();
-        proxyFactory.setInterfaces(new Class[] {CountService.class});
-        Class<?> proxyClass = proxyFactory.createClass();
-        CountService javaassistProxy = (CountService)proxyClass.newInstance();
-        ((ProxyObject)javaassistProxy).setHandler(new JavaAssistInterceptor(delegate));
-        return javaassistProxy;
-    }
-
-    /****************************/
-
-    /**
-     * JavaAssist byte code
-     */
-    private static CountService createJavaAssistByteCodeDynamicProxy(CountService delegate) throws Exception {
-        ClassPool pool = new ClassPool(true);
-        CtClass ctc = pool.makeClass(CountService.class.getName() + "JavaAssistProxy");
-        ctc.addInterface(pool.get(CountService.class.getName()));
-        ctc.addConstructor(CtNewConstructor.defaultConstructor(ctc));
-        ctc.addField(CtField.make("public " + CountService.class.getName() + " delegate;", ctc));
-        ctc.addMethod(CtNewMethod.make(
-            "public int count() {return delegate.count();}",
-            ctc));
-        Class<?> proxyClass = ctc.toClass();
-        CountService bytecodeProxy = (CountService)proxyClass.newInstance();
-        Field field = bytecodeProxy.getClass().getField("delegate");
-        field.set(bytecodeProxy, delegate);
-        return bytecodeProxy;
-    }
-
-}
+//package cn.su.study.proxy;
+//
+//import javassist.*;
+//import javassist.util.proxy.MethodHandler;
+//import javassist.util.proxy.ProxyFactory;
+//import javassist.util.proxy.ProxyObject;
+//import org.springframework.cglib.proxy.Enhancer;
+//import org.springframework.cglib.proxy.MethodInterceptor;
+//import org.springframework.cglib.proxy.MethodProxy;
+//
+//import java.lang.reflect.Field;
+//import java.lang.reflect.InvocationHandler;
+//import java.lang.reflect.Method;
+//import java.lang.reflect.Proxy;
+//import java.text.DecimalFormat;
+//
+///**
+// * @author suyulong
+// * @date 2018/5/22 16:01
+// */
+//public class DynamicProxyPerformanceTest {
+//
+//    /**
+//     *
+//     * Create JDK Proxy: 7 ms
+//     * Create CGLIB Proxy: 100 ms
+//     * Create JAVAASSIST Proxy: 88 ms
+//     * Create JAVAASSIST Bytecode Proxy: 59 ms
+//     * ================
+//     * Run JDK Proxy: 44 ms, 32,046,941 t/s
+//     * Run CGLIB Proxy: 169 ms, 8,343,582 t/s
+//     * Run JAVAASSIST Proxy: 211 ms, 6,682,774 t/s
+//     * Run JAVAASSIST Bytecode Proxy: 52 ms, 27,116,642 t/s
+//     *
+//     * @param args
+//     * @throws Exception
+//     */
+//
+//    public static void main(String[] args) throws Exception {
+//        CountService delegate = new CountServiceImpl();
+//
+//        long time = System.currentTimeMillis();
+//        CountService jdkProxy = createJdkDynamicProxy(delegate);
+//        time = System.currentTimeMillis() - time;
+//        System.out.println("Create JDK Proxy: " + time + " ms");
+//
+//        time = System.currentTimeMillis();
+//        CountService cglibProxy = createCglibDynamicProxy(delegate);
+//        time = System.currentTimeMillis() - time;
+//        System.out.println("Create CGLIB Proxy: " + time + " ms");
+//
+//        time = System.currentTimeMillis();
+//        CountService javassistProxy = createJavaAssistDynamicProxy(delegate);
+//        time = System.currentTimeMillis() - time;
+//        System.out.println("Create JAVAASSIST Proxy: " + time + " ms");
+//
+//        time = System.currentTimeMillis();
+//        CountService javassistBytecodeProxy = createJavaAssistByteCodeDynamicProxy(delegate);
+//        time = System.currentTimeMillis() - time;
+//        System.out.println("Create JAVAASSIST Bytecode Proxy: " + time + " ms");
+//
+//        System.out.println("================");
+//
+//        for (int i = 0; i < 3; i++) {
+//            test(jdkProxy, "Run JDK Proxy: ");
+//            test(cglibProxy, "Run CGLIB Proxy: ");
+//            test(javassistProxy, "Run JAVAASSIST Proxy: ");
+//            test(javassistBytecodeProxy, "Run JAVAASSIST Bytecode Proxy: ");
+//            System.out.println("----------------");
+//        }
+//
+//    }
+//
+//    private static void test(CountService service, String label)
+//        throws Exception {
+//        service.count(); // warm up
+//        int count = 10000000;
+//        long time = System.currentTimeMillis();
+//        for (int i = 0; i < count; i++) {
+//            service.count();
+//        }
+//        time = System.currentTimeMillis() - time;
+//        System.out.println(label + time + " ms, " + new DecimalFormat().format(count * 1000 / time) + " t/s");
+//    }
+//
+//
+//    /**
+//     * jdk dynamic proxy
+//     * <p>
+//     * JDK的动态代理是靠反射完成的 一定要实现接口
+//     *
+//     * @param delegate
+//     * @return
+//     */
+//    private static CountService createJdkDynamicProxy(final CountService delegate) {
+//        CountService countService_jdk = (CountService)Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(),
+//            new Class[] {CountService.class},
+//            new JdkHandler(delegate));
+//        return countService_jdk;
+//    }
+//
+//    private static class JdkHandler implements InvocationHandler {
+//
+//        final Object delegate;
+//
+//        private JdkHandler(Object delegate) {this.delegate = delegate;}
+//
+//        @Override
+//        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//            //long time = System.currentTimeMillis();
+//            Object o = method.invoke(delegate, args);
+//            //time = System.currentTimeMillis() - time;
+//            //System.out.println("Create JDK Proxy: " + time + " ms");
+//            return o;
+//        }
+//    }
+//
+//    /*****************************/
+//
+//    /**
+//     * cglib dynamic proxy
+//     * <p>
+//     * cglib原理是让目标类生成一个子类，然后让子类去进行方法的增强
+//     * <p>
+//     * final Class不支持, 因为CGLIB是生成子类来实现AOP,所以final Class自然无法支持了. 需要强制无参数构造函数
+//     *
+//     * @param delegate
+//     * @return
+//     */
+//    private static CountService createCglibDynamicProxy(final CountService delegate) {
+//        Enhancer enhancer = new Enhancer();
+//        enhancer.setCallback(new CglibInterceptor(delegate));
+//        enhancer.setInterfaces(new Class[] {CountService.class});
+//        CountService cglibProxy = (CountService)enhancer.create();
+//        return cglibProxy;
+//    }
+//
+//    /**
+//     * MethodInterceptor接口只定义了一个方法：
+//     * <p>
+//     * public Object intercept(Object object, java.lang.reflect.Method method, Object[] args, MethodProxy proxy) throws
+//     * Throwable;
+//     * <p>
+//     * 参数Object object是被代理对象，不会出现死循环的问题。
+//     * <p>
+//     * 参数java.lang.reflect.Method method是java.lang.reflect.Method类型的被拦截方法。
+//     * <p>
+//     * 参数Object[] args是被被拦截方法的参数。
+//     * <p>
+//     * 参数MethodProxy proxy是CGLIB提供的MethodProxy 类型的被拦截方法。
+//     * <p>
+//     * 注意：
+//     * <p>
+//     * 1、若原方法的参数存在基本类型，则对于第三个参数Object[] args会被转化成类的类型。如原方法的存在一个参数为int，则在intercept方法中，对应的会存在一个Integer类型的参数。
+//     * <p>
+//     * 2、若原方法为final方法，则MethodInterceptor接口无法拦截该方法
+//     */
+//    private static class CglibInterceptor implements MethodInterceptor {
+//        final Object delegate;
+//
+//        private CglibInterceptor(Object delegate) {this.delegate = delegate;}
+//
+//        @Override
+//        public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+//            //long time = System.currentTimeMillis();
+//            Object obj = method.invoke(delegate, objects);
+//            //time = System.currentTimeMillis() - time;
+//            //System.out.println("Create CGLib Proxy: " + time + " ms");
+//            return obj;
+//        }
+//    }
+//
+//    /****************************/
+//
+//    /**
+//     * JavaAssist 类似jdk接口实现 动态代理
+//     */
+//
+//    private static class JavaAssistInterceptor implements MethodHandler {
+//        final Object delegate;
+//
+//        private JavaAssistInterceptor(Object delegate) {this.delegate = delegate;}
+//
+//        @Override
+//        public Object invoke(Object o, Method method, Method method1, Object[] objects) throws Throwable {
+//            //long time = System.currentTimeMillis();
+//            Object obj = method.invoke(delegate, objects);
+//            //time = System.currentTimeMillis() - time;
+//            //System.out.println("Create JAVAASSIST Proxy: " + time + " ms");
+//            return obj;
+//        }
+//    }
+//
+//    private static CountService createJavaAssistDynamicProxy(final CountService delegate)
+//        throws IllegalAccessException, InstantiationException {
+//        ProxyFactory proxyFactory = new ProxyFactory();
+//        proxyFactory.setInterfaces(new Class[] {CountService.class});
+//        Class<?> proxyClass = proxyFactory.createClass();
+//        CountService javaassistProxy = (CountService)proxyClass.newInstance();
+//        ((ProxyObject)javaassistProxy).setHandler(new JavaAssistInterceptor(delegate));
+//        return javaassistProxy;
+//    }
+//
+//    /****************************/
+//
+//    /**
+//     * JavaAssist byte code
+//     */
+//    private static CountService createJavaAssistByteCodeDynamicProxy(CountService delegate) throws Exception {
+//        ClassPool pool = new ClassPool(true);
+//        CtClass ctc = pool.makeClass(CountService.class.getName() + "JavaAssistProxy");
+//        ctc.addInterface(pool.get(CountService.class.getName()));
+//        ctc.addConstructor(CtNewConstructor.defaultConstructor(ctc));
+//        ctc.addField(CtField.make("public " + CountService.class.getName() + " delegate;", ctc));
+//        ctc.addMethod(CtNewMethod.make(
+//            "public int count() {return delegate.count();}",
+//            ctc));
+//        Class<?> proxyClass = ctc.toClass();
+//        CountService bytecodeProxy = (CountService)proxyClass.newInstance();
+//        Field field = bytecodeProxy.getClass().getField("delegate");
+//        field.set(bytecodeProxy, delegate);
+//        return bytecodeProxy;
+//    }
+//
+//}

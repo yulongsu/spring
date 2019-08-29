6bf445908f3d5a5cbda250af2545311557f5c406
suyulong <hiro.syl@alibaba-inc.com>
2019-08-29 17:59:59 +0800

    fix

--- src/main/java/cn/su/daily/CSVProcess.java
+++ src/main/java/cn/su/daily/CSVProcess.java
@@ -4,13 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
+import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
@@ -20,16 +18,18 @@ import com.opencsv.CSVReaderBuilder;
  */
 public class CSVProcess {
 
-    public  Map<String,String> read(){
+    public Map<String, String> read() {
         String filePath = this.getClass().getClassLoader().getResource("param.csv").getPath();
-        try (CSVReader csvReader = new CSVReaderBuilder(new BufferedReader(new InputStreamReader(new FileInputStream(new File(filePath)), "utf-8"))).build()) {
+        try (CSVReader csvReader = new CSVReaderBuilder(
+            new BufferedReader(new InputStreamReader(new FileInputStream(new File(filePath)), "utf-8")))
+            .withCSVParser(new CSVParserBuilder().withSeparator('#').build()).build()) {
             Iterator<String[]> iterator = csvReader.iterator();
-            Map<String,String> map = new HashMap<>();
+            Map<String, String> map = new HashMap<>();
             //skip title
             iterator.next();
             while (iterator.hasNext()) {
                 String[] strArr = iterator.next();
-                map.put(strArr[0],strArr[1]);
+                map.put(strArr[0], strArr[1]);
             }
             return map;
         } catch (Exception e) {
